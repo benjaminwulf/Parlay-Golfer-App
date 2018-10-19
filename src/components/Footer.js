572a8b93ee5d05d26d30bf03208c7599ed358164
Benjamin W Wulf <benjaminwulf@gmail.com>
2018-10-18 18:23:52 -0600

    last hole, next hole

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,14 @@ import React from "react";
 
 const Footer = props => (
   <div className="card-body">
-    <p className="card-text">Click Count: {props.count}</p>
+    <p className="card-text"> {props.count}</p>
     <button className="btn btn-danger" onClick={props.handleDecrement}>
-      Last Hole
+    <i class="fas fa-angle-double-left"></i>
+    Last Hole
     </button>
     <button className="btn btn-primary" onClick={props.handleIncrement}>
-      Next Hole
+    Next Hole
+    <i class="fas fa-angle-double-right"></i>
     </button>{" "}
   </div>
 );
